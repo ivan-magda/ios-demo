cb4ea9d61aaee9e301a22727f9b0729e351887d0
Ivan Magda <imagda15@gmail.com>
2017-11-21 18:07:47 +0300

    [IntroductionAutoLayout] fix: Typo

--- IntroductionAutoLayout/IntroductionAutoLayout/Sources/ViewControllers/SwipingCollectionViewController.swift
+++ IntroductionAutoLayout/IntroductionAutoLayout/Sources/ViewControllers/SwipingCollectionViewController.swift
@@ -25,7 +25,7 @@ import UIKit
 private let reuseIdentifier = "cellId"
 
 private let pages = [
-  Page(imageName: "bear_first", headerText: "Join use today in our fun and games!",
+  Page(imageName: "bear_first", headerText: "Join us today in our fun and games!",
           bodyText: "Are you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon."),
   Page(imageName: "heart_second", headerText: "Subscribe and get coupons on our daily events",
           bodyText: "Get notified of the savings immediately when we announce them on our website. Make sure to also give us any feedback you have."),
