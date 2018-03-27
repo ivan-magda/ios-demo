ce68f80df9858a37308cb4174e8060ef9a6e1f5f
Ivan Magda <imagda15@gmail.com>
2018-03-27 10:33:37 +0300

    [PanGestureRecognizer]: Initial commit

---
+++ PanGestureRecognizer/PanGestureRecognizer/Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PanGestureRecognizer
+//
+//  Created by Ivan Magda on 27/03/2018.
+//  Copyright © 2018 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+// MARK: AppDelegate: UIResponder, UIApplicationDelegate
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    // MARK: Instance Variables
+
+    var window: UIWindow?
+
+    // MARK: UIApplicationDelegate
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
---
+++ PanGestureRecognizer/PanGestureRecognizer/Sources/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PanGestureRecognizer
+//
+//  Created by Ivan Magda on 27/03/2018.
+//  Copyright © 2018 Ivan Magda. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // MARK: UIViewController lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
